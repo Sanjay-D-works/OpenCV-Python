f9e3e8b34ed4df942b2d928cd52a7b5265d65d93
Sanjay-D-works <sanjaydevarajworks@gmail.com>
2025-06-30 13:32:27 +0530

    OpenCV-Python Project

---
+++ draw.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((500,500,3), dtype='uint8')
+cv.imshow('Blank',blank)
+
+# img = cv.imread('picture/img 3.jpg')
+# cv.imshow('Cat', img)
+
+# 1. Paint the image a certain colour
+# blank[200:300, 300:400] = 0,0,255
+# cv.imshow('Green',blank)
+
+
+# 2. Draw a Rectangle.
+# """img.shape[1] // 2 , img.shapr[0] // 2"""
+
+cv.rectangle(blank, (0,0), (250,500), (0,255,0), thickness=cv.FILLED)
+cv.imshow('Rectangle', blank)
+
+
+# 3. Draw A circle
+cv.circle(blank, (blank.shape[1] // 2 , blank.shape[0] // 2) , 40, (0,0,255),
+          thickness=-1)
+cv.imshow('Circle', blank)
+
+# 4. Draw a line.
+cv.line(blank, (0,0), (300,400), 40, (255,255,255),thickness=3)
+
+cv.imshow('Line', blank)
+
+
+cv.waitKey(0)
+
+
---
+++ read.py
@@ -0,0 +1,28 @@
+
+# Reading the image
+
+import cv2 as cv
+
+img = cv.imread('picture/img 2_large.jpg')
+
+cv.imshow('Dog-Cat', img)
+
+
+cv.waitKey(0)
+
+# Reading the video
+
+# capture = cv.VideoCapture('videos/20220611_135309.mp4')
+#
+#  while True:
+#      isTrue, frame = capture.read()
+#      cv.imshow('videos', frame)
+#
+#      if cv.waitKey(20) & 0xFF==ord('d'):
+#          break
+#
+#  capture.release()
+#  cv.destroyAllWindows()
+
+
+
