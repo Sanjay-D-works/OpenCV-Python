5091eac9744d76fd07005d01b017e25c18a7a5db
Sanjay-D-works <sanjaydevarajworks@gmail.com>
2025-07-08 12:43:46 +0530

    How to identify the person using face recognition

---
+++ 15. haar_face.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+
+img = cv.imread('picture/cutie 2.jpg')
+cv.imshow('cutie', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray person', gray)
+
+haar_cascade = cv.CascadeClassifier('15.1. haar_face.xml')
+
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6)
+
+print(f'Number of faces found = {len(faces_rect)}')
+
+for x,y,z,h in faces_rect:
+    cv.rectangle(img,(x,y),(x+z,y+h),(255,0,0),2)
+
+cv.imshow('Detected faces', img)
+
+img = cv.imread('picture/people.jpg')
+cv.imshow('Group of people', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray person', gray)
+
+haar_cascade = cv.CascadeClassifier('15.1. haar_face.xml')
+
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2)
+
+print(f'Number of faces found = {len(faces_rect)}')
+
+for x,y,z,h in faces_rect:
+    cv.rectangle(img,(x,y),(x+z,y+h),(255,0,0),2)
+
+cv.imshow('Detected faces', img)
+
+cv.waitKey(0)
